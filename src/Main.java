4d7d96c1b246bc03ef8afac16e3bd7e4bc9f82e7
dfirmin <deantedfirmin@gmail.com>
2017-10-30 09:39:28 -0400

    initial commit Lab 6

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,30 @@
 import java.util.Scanner;
 
-
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
 		String userInput = getUserInput(scnr);
 		System.out.println(userInput);
-		System.out.println(startsWithVowel(userInput));
+		//System.out.println(startsWithVowel(userInput));
 		System.out.println(convertWord(userInput));
-		
+
 	}
-	
-	//get user input
+
+	// get user input
 	public static String getUserInput(Scanner scanner) {
 		System.out.println("Type your string to be converted");
 		return scanner.nextLine();
 	}
+
 	// check if first level starts with a vowel.
 	public static boolean startsWithVowel(String input) {
 		return "AEIOUaeiou".indexOf(input.charAt(0)) != -1;
 	}
-	//convert the word to pig latin by using StringBuilder as the new pig latin string
-	//returns the StringBuilder string as a normal string.
+
+	// convert the word to pig latin by using StringBuilder as the new pig latin
+	// string
+	// returns the StringBuilder string as a normal string.
 	public static String convertWord(String input) {
 		StringBuilder modifiedInput = new StringBuilder();
 		 
@@ -33,16 +35,19 @@ public class Main {
 				//FIX ME, THIS IS ONLY MOVING THE FIRST CHARACTER TO THE END OF STRING
 				// SOME TYPE OF LOOP IS NEEDED
 				else {
+					for (int i = 0; i < word.length(); i++) {
+						if ("AEIOUaeiou".indexOf(word.charAt(i)) > -1) {
+							modifiedInput.append(word.substring(i, word.length()) + word.substring(0, i)+ "ay ");
+							
+						}
+					}
 				
-					//modifiedInput.append(word.substring(word.indexOf("AEIOUaeiou")));
-					modifiedInput.append(word.substring(1)).append(word.charAt(0)).append("ay ");
+					//modifiedInput.append(word.substring(1)).append(word.charAt(0)).append("ay ");
 				}
 			}
 			
 		
 		return modifiedInput.toString();
 	}
-	
-	
 
 }
